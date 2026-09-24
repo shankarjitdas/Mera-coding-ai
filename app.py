a1bff2f8d209042fb28f43e5e94bc0be29e771c4
shankarjitdas <shankarjitdas2@gmail.com>
2026-09-24 23:57:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="DasAi", page_icon="🤖")
 st.title("🤖 DasAi - Coding Assistant")
 st.write("Aapka apna AI assistant, jo bina kisi memory limit ke fast chalega!")
 
-HF_TOKEN = "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"
+HF_TOKEN = "aiqcpWLhRPTvlSDvllYlmBAtcoTzQrFBWb"
 
 # Client setup
 os.environ["HF_TOKEN"] = HF_TOKEN
