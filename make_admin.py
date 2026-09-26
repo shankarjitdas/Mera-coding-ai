cbfd0a7cf4674821311f43ef1946dcdfa58ed339
DasAi <shankarjitdas2@gmail.com>
2026-09-26 22:44:58 +0530

    Update make_admin.py

--- make_admin.py
+++ make_admin.py
@@ -1,1 +1,13 @@
+import sqlite3
+
+# Database se connect karein
+conn = sqlite3.connect("dasai_professional.db")
+c = conn.cursor()
+
+# Aapke email ko Admin set karne ki query
+c.execute("UPDATE users SET is_admin = 1 WHERE email = 'shankarjitdas2@gmail.com'")
+conn.commit()
+conn.close()
+
+print("Success! shankarjitdas2@gmail.com ab Admin ban chuka hai.")
 
